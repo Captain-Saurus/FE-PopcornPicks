cb9465091dab91e1b6664e47a21c6bbe52e65239
pangkyu <baesense@naver.com>
2023-09-19 20:32:26 +0900

    Design: 예약페이지 UI 수정
    UI 디자인 변경에 맞게 수정처리
    현재 모바일뷰만 대응

--- popcorn-picks/src/styles/components/Reservation.tsx
+++ popcorn-picks/src/styles/components/Reservation.tsx
@@ -10,17 +10,22 @@ const Reservation = () => {
             return <ProcessDiv key={index}>{item}</ProcessDiv>;
           })}
         </ProcessSection>
-        <TheaterSection>
-          <TheaterSelectedTitle>선택한 영화관</TheaterSelectedTitle>
-          <TheaterRegionDiv>
-            <RegionDiv>
-              {REGION.map((item, index: number) => {
-                return <RegionDiv key={index}>{item}</RegionDiv>;
-              })}
-            </RegionDiv>
-            <TheaterListDiv>영화관 리스트</TheaterListDiv>
-          </TheaterRegionDiv>
-        </TheaterSection>
+        <TheaterRegionDiv>
+          <RegionDiv>
+            {REGION.map((item, index: number) => {
+              return <RegionDiv key={index}>{item}</RegionDiv>;
+            })}
+          </RegionDiv>
+        </TheaterRegionDiv>
+        <TheaterListDiv>영화관 리스트(가로 스크롤)</TheaterListDiv>
+        <MovieListDiv>
+          <p>이미지만 보여주기 </p>
+        </MovieListDiv>
+        <MovieCalendarDiv>
+          <p>캘린더</p>
+        </MovieCalendarDiv>
+        <p>선택한 영화명 </p>
+        <MovieTimeDiv></MovieTimeDiv>
       </ReservationSection>
     </>
   );
@@ -30,17 +35,17 @@ export default Reservation;
 
 const ReservationSection = styled.section`
   display: flex;
+  flex-direction: column;
   width: 99%;
-  height: 40vh;
-  margin: 0 auto;
+  height: 70vh;
+  margin: 2% auto;
   border: 1px solid black;
   font-size: 10px;
 `;
 
 const ProcessSection = styled.section`
   display: flex;
-  flex-direction: column;
-  width: 10%;
+  height: 40px;
 `;
 
 const ProcessDiv = styled.div`
@@ -51,36 +56,43 @@ const ProcessDiv = styled.div`
   border: 1px solid black;
 `;
 
-const TheaterSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  width: 28%;
-  border: 1px solid black;
-`;
-
-const TheaterSelectedTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 30px;
-  border: 1px solid black;
-`;
-
 const TheaterRegionDiv = styled.div`
   display: flex;
   flex-direction: row;
-  flex-grow: 1;
+  height: 40px;
+  border-bottom: 1px solid black;
 `;
 
 const RegionDiv = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   flex-grow: 1;
   font-size: 10px;
+  align-items: center;
 `;
 
 const TheaterListDiv = styled.div`
   display: flex;
   border: 1px solid black;
+  height: 40px;
+  align-items: center;
+  justify-contents: center;
+`;
+
+const MovieListDiv = styled.div`
+  display: flex;
+  align-items: center;
+  height: 150px;
+  border-bottom: 1px solid black;
+`;
+
+const MovieCalendarDiv = styled.div`
+  display: flex;
+  height: 80px;
+  align-items: center;
+  border-bottom: 1px solid black;
+`;
+
+const MovieTimeDiv = styled.div`
+  display: flex;
 `;
